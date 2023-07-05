74acaa67c2b576c6d3880165af26aa4f7b353e0a
nxnikolaos <nxnatlas@gmail.com>
2023-07-05 15:38:15 +0300

    Connected the TMDB.

--- src/data/Data.js
+++ src/data/Data.js
@@ -3,33 +3,22 @@ import { useState, useEffect } from "react";
 const Data = () => {
   const [movies, setMovies] = useState([]);
 
-  async function fetchData() {
-    const url = "https://imdb-top-100-movies.p.rapidapi.com/top17";
+  useEffect(() => {
     const options = {
       method: "GET",
       headers: {
-        "X-RapidAPI-Key": "3bbe1e218emsh33ff615efc7b1d9p12f696jsnf5ea644365e0",
-        "X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com",
+        accept: "application/json",
+        Authorization: "zaza",
       },
     };
 
-    try {
-      const response = await fetch(url, options);
-      const result = await response.text();
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    fetchData();
-
-    // fetch(url, options)
-    //   .then((response) => response.json())
-    //   .then((data) => console.log(data))
-    //   .then((response) => console.log(response))
-    //   .catch((err) => console.error(err));
+    fetch(
+      "https://api.themoviedb.org/3/person/popular?language=en-US&page=1",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
   }, []);
   return <></>;
 };
