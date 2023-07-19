13aff3df58d117d6670e0ae8c9198afbacf1ee35
nxnikolaos <nxnatlas@gmail.com>
2023-07-19 13:11:19 +0300

    Home page redeseign.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import Content from "../components/Content";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
+      <Header />
       <Content />
+      <Footer />
     </>
   );
 };
