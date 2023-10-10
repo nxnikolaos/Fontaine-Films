816cb12983d1ab33b142b1b30b8de89370d34841
nxnikolaos <nxnatlas@gmail.com>
2023-10-10 16:05:39 +0300

    Removed links from Menu component.

--- frontend/src/components/navigation/Menu.js
+++ frontend/src/components/navigation/Menu.js
@@ -7,14 +7,9 @@ const Menu = () => {
         <li>
           <Link to={"/"}>Home</Link>
         </li>
-        <li>
-          <a href="#">Movies</a>
-        </li>
+
         <li>
           <Link to={"/about"}>About</Link>
-        </li>
-        <li>
-          <a href="#">Contact</a>
         </li>
       </ul>
     </div>
