48297a06ec7cdd4977822e6a9017acc1feeab333
nxnikolaos <nxnatlas@gmail.com>
2023-10-24 13:28:54 +0300

    Search query parameters base.

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,10 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 
-const movieUrl = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
-const imgConfigUrl = "https://api.themoviedb.org/3/configuration";
+const baseUrl = `https://api.themoviedb.org/3/`;
+const movieUrl = `${baseUrl}discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
+const imgConfigUrl = `${baseUrl}configuration`;
+const queryUrl = `https://api.themoviedb.org/3/search/movie?query=${lord}&include_adult=false&language=en-US&page=1`;
 
 app.use(express.static("build"));
 
@@ -48,6 +50,28 @@ app.get("/api/movies", (req, res) => {
     .request(options)
     .then(function (response) {
       res.json(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
+
+/*search query request */
+
+app.get("/api/movies/search", (req, res) => {
+  const options = {
+    method: "GET",
+    url: queryUrl,
+    headers: {
+      accept: "application/json",
+      Authorization: "Bearer " + process.env.REACT_APP_API_KEY,
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
     })
     .catch(function (error) {
       console.error(error);
