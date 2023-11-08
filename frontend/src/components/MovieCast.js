3c730bd280abec757459286d538e9991c5530b41
nxnikolaos <nxnatlas@gmail.com>
2023-11-08 16:27:44 +0200

    Added a blank image in case of blank actor image.

--- frontend/src/components/MovieCast.js
+++ frontend/src/components/MovieCast.js
@@ -1,4 +1,5 @@
 import React from "react";
+import blank_profile from "../assets/images/blank-profile.png";
 
 export const MovieCast = ({ movie, imgPath }) => {
   return (
@@ -9,13 +10,18 @@ export const MovieCast = ({ movie, imgPath }) => {
           movie.credits.cast.slice(0, 10).map((actor) => {
             return (
               <article className="text-center" key={actor.id}>
-                {actor.profile_path && (
-                  <img
-                    className="actor-profile-pic"
-                    src={imgPath + actor.profile_path}
-                    alt={"Thumbnail of " + actor.original_name}
-                  ></img>
-                )}
+                {/* {actor.profile_path && ( */}
+                <img
+                  className="actor-profile-pic"
+                  // src={imgPath + actor.profile_path}
+                  src={
+                    actor.profile_path
+                      ? imgPath + actor.profile_path
+                      : blank_profile
+                  }
+                  alt={"Thumbnail of " + actor.original_name}
+                ></img>
+                {/* )} */}
                 <p> {actor.original_name}</p>
                 <p>as</p>
                 <p>{actor.character}</p>
